f70aeaa92af6457f2be47578a74903eccf4243ca
foxadpatres@gmail.com <foxadpatres@gmail.com@749de6a6-f9d6-11de-8173-5df4dc7aba59>
2010-08-24 20:10:16 +0000

    This is the first combine.c It's just for you to have it if you need it, have some idea etc ... You can compile it, but it's all comments ;) You can see that i started to create some combinaison, but it's all in real language, i'll code it really later Atm, it's just combining useless things into another useless things.

---
+++ combine.c
@@ -0,0 +1,81 @@
+/*
+ * combine.c
+ *
+ *  Created on: 24 août 2010
+ *      Author: aiseant
+ */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "mud.h"
+
+
+void do_combine(CHAR_DATA * ch ,OBJ_DATA * source1, OBJ_DATA* source2 )
+{
+
+	//verify source1 and source2 existing in inv of ch;
+
+	//message of beginning;
+
+	/*switch( type de source 1)
+	{
+
+		case SLAG ://115
+			case  source2SLAG://115
+				destroy sources;
+				create FRAGMENT;/116
+			break
+
+			case source2FRAGMENT://116
+				detsroy sources;
+				create FILES;//120
+			break;
+			default : break;
+		break;
+
+
+		case FRAGMENT ://116
+			case source2SLAG:
+				destroy sources;
+				create FILE;//117
+			break
+
+			case source2FRAGMENT://116
+				detsroy sources;
+				create FILES;//117
+			break;
+			default:break;
+		break;
+
+		case PASSWORD ://118
+			case source2LOGIN://119
+				destroy sources;
+				create IDENTIFICATION;//120
+			break;
+			default: break;
+		break;
+
+		case LOGIN : //119
+			case source2PASSWORD: //118
+				destroy sources;
+				create IDENTIFICATION;//120
+			break;
+			default: break;
+		break;
+
+		case BLOB: //121
+			destroy sources;
+			("man, he just eat all and run away !");
+		break;
+
+		case THING:
+		break;
+
+		default :
+			message of impossible;
+		break;
+
+	}*/
+}
